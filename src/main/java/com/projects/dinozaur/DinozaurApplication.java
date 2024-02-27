c5bcc335306bcf71b07ad9cbf9da083a0acc589f
Zaur <bvadres@rambler.ru>
2024-02-27 15:51:33 +0200

    initial commit

---
+++ src/main/java/com/projects/dinozaur/DinozaurApplication.java
@@ -0,0 +1,13 @@
+package com.projects.dinozaur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinozaurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DinozaurApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projects/dinozaur/DinozaurApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.dinozaur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DinozaurApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
