a93d9f3a8318495ac5ec3d5419ae512fa90ae6e2
Zaur <bvadres@rambler.ru>
2024-02-27 20:02:23 +0200

    Заполнение классов

--- src/main/java/com/projects/dinozaur/Group.java
+++ src/main/java/com/projects/dinozaur/Group.java
@@ -1,5 +1,41 @@
 package com.projects.dinozaur;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
+@Entity
 public class Group {
+    @Id
+    @GeneratedValue
+    int id;
+    @Column
+    int studentId;
+    @Column
+    String groupName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
 }
--- src/main/java/com/projects/dinozaur/Student.java
+++ src/main/java/com/projects/dinozaur/Student.java
@@ -1,4 +1,62 @@
 package com.projects.dinozaur;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Student {
+    @Id
+    int id;
+    @Column
+    String name;
+    @Column
+    String surname;
+    @Column
+    int sex;
+    @Column
+    boolean active;
+    @ManyToOne
+    Group group;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getSex() {
+        return sex;
+    }
+
+    public void setSex(int sex) {
+        this.sex = sex;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
 }
