0f376e70b39e9650e37303485b1faa623ddfa438
Zaur <bvadres@rambler.ru>
2024-04-02 15:58:01 +0200

    Исправление ошибок, доработка зависимостей

--- src/main/java/com/projects/dinozaur/Group.java
+++ src/main/java/com/projects/dinozaur/Group.java
@@ -1,41 +1,18 @@
 package com.projects.dinozaur;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
+@Table(name="groups")
 public class Group {
     @Id
     @GeneratedValue
-    int id;
-    @Column
+    Integer id;
+    @Column(name = "student-id")
     int studentId;
-    @Column
+    @Column(name = "group-name")
     String groupName;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public int getStudentId() {
-        return studentId;
-    }
-
-    public void setStudentId(int studentId) {
-        this.studentId = studentId;
-    }
-
-    public String getGroupName() {
-        return groupName;
-    }
-
-    public void setGroupName(String groupName) {
-        this.groupName = groupName;
-    }
+    @OneToMany(mappedBy = "student-id")
+    List<Student> students;
 }
--- src/main/java/com/projects/dinozaur/Student.java
+++ src/main/java/com/projects/dinozaur/Student.java
@@ -1,11 +1,9 @@
 package com.projects.dinozaur;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 
 @Entity
+@Table(name="students")
 public class Student {
     @Id
     int id;
@@ -18,45 +16,8 @@ public class Student {
     @Column
     boolean active;
     @ManyToOne
+    @JoinColumn
     Group group;
 
-    public int getId() {
-        return id;
-    }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public int getSex() {
-        return sex;
-    }
-
-    public void setSex(int sex) {
-        this.sex = sex;
-    }
-
-    public boolean isActive() {
-        return active;
-    }
-
-    public void setActive(boolean active) {
-        this.active = active;
-    }
 }
